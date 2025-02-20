af5f6c791c15bc39bd5b936ab2433b008b4a7b2e
Abdullah Bukhari <abdullah.bukhari@emumba.com>
2025-02-20 14:59:11 +0500

    following best preactice for naming observable variables

--- src/app/core/todo.service.ts
+++ src/app/core/todo.service.ts
@@ -3,50 +3,50 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { Todo } from '../todo/_models/todo';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
   private todos: Todo[] = [];
   private todosSubject = new BehaviorSubject<Todo[]>([]);
-
+  private todo$: any = this.todosSubject.asObservable();
   constructor() {
     const savedTodos = localStorage.getItem('todos');
     if (savedTodos) {
       this.todos = JSON.parse(savedTodos);
-      this.todosSubject.next(this.todos);
+      this.todo$.next(this.todos);
     }
   }
 
   getTodos(): Observable<Todo[]> {
-    return this.todosSubject.asObservable();
+    return this.todo$;
   }
 
   addTodo(text: string): void {
     const newTodo: Todo = {
       id: crypto.randomUUID(),
       text: text.trim(),
-      completed: false
+      completed: false,
     };
     this.todos = [newTodo, ...this.todos];
     this.updateTodos();
   }
 
   toggleTodo(id: string): void {
-    this.todos = this.todos.map(todo =>
+    this.todos = this.todos.map((todo) =>
       todo.id === id ? { ...todo, completed: !todo.completed } : todo
     );
     this.updateTodos();
   }
 
   updateTodoText(id: string, text: string): void {
-    this.todos = this.todos.map(todo =>
+    this.todos = this.todos.map((todo) =>
       todo.id === id ? { ...todo, text: text.trim() } : todo
     );
     this.updateTodos();
   }
 
   deleteTodo(id: string): void {
-    this.todos = this.todos.filter(todo => todo.id !== id);
+    this.todos = this.todos.filter((todo) => todo.id !== id);
     this.updateTodos();
   }
 
